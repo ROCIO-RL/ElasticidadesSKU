e8413f5bb3ee32be958c471fb85bb8506eb7aa30
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-10-17 16:56:11 +0000

    insight con df

--- ElasticidadSKU.py
+++ ElasticidadSKU.py
@@ -475,7 +475,7 @@ class ElasticidadCB:
 
         client = OpenAI(
             base_url="https://router.huggingface.co/v1",
-            api_key=st.secrets["HUGGINGFACE"]["HF_TOKEN"],
+            api_key=st.secrets["HUGGINGFACE"]["HF_TOKEN_2"],
         )
 
         completion = client.chat.completions.create(
