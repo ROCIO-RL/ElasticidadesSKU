2086270999bfd3cc622379615a48b2ee18e983f6
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-11-07 16:09:51 +0000

    ajustes esteticos

--- ElasticidadSKU_streamlit.py
+++ ElasticidadSKU_streamlit.py
@@ -756,11 +756,11 @@ if layout is not None and st.button("Ejecutar Análisis"):
         
                 col1, col2 = st.columns([6, 1])  # Ajusta proporciones si quieres que el logo sea más pequeño
 
-                with col1:
+                with col2:
                     st.markdown("## Insight")
 
-                with col2:
-                    st.image("Logo2.png", width=60)  # Ajusta el ancho según tu logo
+                with col1:
+                    st.image("Logo2.png", width=100)  # Ajusta el ancho según tu logo
                 st.markdown(
                             f"""
                             <div style="
