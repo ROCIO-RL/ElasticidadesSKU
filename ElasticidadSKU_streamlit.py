afaba67033ddff7e4ce23f21b0fbb2a6f83266fa
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-11-07 16:29:40 +0000

    ajustes esteticos

--- ElasticidadSKU_streamlit.py
+++ ElasticidadSKU_streamlit.py
@@ -755,7 +755,7 @@ if layout is not None and st.button("Ejecutar Análisis"):
                 
                 col1, col2 = st.columns(2)
                 with col1:
-                    st.image("logo.png", width=100) 
+                    st.image("logo.png", width=50) 
                 with col2:    
                     st.markdown("## Insight")
 
