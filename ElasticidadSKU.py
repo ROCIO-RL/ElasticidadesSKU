3957ba3e155396367b9594295c450f9bdbfcb8c9
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-11-10 22:49:37 +0000

    ajustes necesarios

--- ElasticidadSKU.py
+++ ElasticidadSKU.py
@@ -595,7 +595,7 @@ class ElasticidadCB:
         if layout['Precio'].isna().sum() > 30:
             raise ValueError("El dataframe contiene demasiados nulos")
 
-        layout.dropna(inplace=True)
+        #layout.dropna(inplace=True)
 
 
         if self.pais=='México':
