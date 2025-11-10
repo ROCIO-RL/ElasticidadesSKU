537f7171a6b040e94e510aca82d396e3d616cdd1
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-11-10 22:39:48 +0000

    ajustes necesarios

--- ElasticidadSKU.py
+++ ElasticidadSKU.py
@@ -1279,7 +1279,7 @@ class ElasticidadCB:
 
         client = OpenAI(
             base_url="https://router.huggingface.co/v1",
-            api_key=st.secrets["HUGGINGFACE"]["HF_TOKEN_3"],
+            api_key=st.secrets["HUGGINGFACE"]["HF_TOKEN_Apagado"],
         )
 
 
