b6f5cefa1f2dc7b4c14c071cf29c73b8bd4ba914
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-11-12 23:32:38 +0000

    Ajuste grafico de FE tendencia

--- ElasticidadSKU.py
+++ ElasticidadSKU.py
@@ -619,7 +619,7 @@ class ElasticidadCB:
         )'''
 
         #Ajuste
-        grado = 3
+        grado = 5
         coef = np.polyfit(df['Precios'], df['FactorElastico_2'], grado)
         poly = np.poly1d(coef)
         df['TendenciaPolinomica'] = poly(df['Precios'])
