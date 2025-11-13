f7e41b22a3829606dc7f0b476d6d2feb67cbde7f
RocioRL <186742156+ROCIO-RL@users.noreply.github.com>
2025-11-13 09:31:23 -0600

    Apagamos key

--- ElasticidadSKU.py
+++ ElasticidadSKU.py
@@ -1027,7 +1027,7 @@ class ElasticidadCB:
         #status
         client = OpenAI(
             base_url="https://router.huggingface.co/v1",
-            api_key=st.secrets["HUGGINGFACE"]["HF_TOKEN_3"],
+            api_key=st.secrets["HUGGINGFACE"]["HF_TOKEN_Apagado"],
         )
 
 
